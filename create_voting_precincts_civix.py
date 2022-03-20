32035959a33336f7eb0c2d695ef74a0f0cde96b9
Greg Bunce <gbunce@utah.gov>
2022-03-20 12:17:19 -0600

    add comments (stable now)

--- create_voting_precincts_civix.py
+++ create_voting_precincts_civix.py
@@ -1,3 +1,4 @@
+from cgi import print_directory
 import arcpy, os, errno, datetime
 
 #: Notes: use arcgispro-py3
@@ -119,8 +120,9 @@ arcpy.analysis.SpatialJoin("sgid_pcts_dissolved_spjoin6.shp", sgid_schoolboarddi
 r'PRECINCT "PRECINCT" true true false 50 Text 0 0,First,#,"sgid_pcts_dissolved_spjoin6.shp",PRECINCT,0,50;COUNTY_NUM "COUNTY_NUM" true true false 2 Text 0 0,First,#,"sgid_pcts_dissolved_spjoin6.shp",COUNTY_NUM,0,2;COUNTY_NAM "COUNTY_NAM" true true false 100 Text 0 0,First,#,"sgid_pcts_dissolved_spjoin6.shp",COUNTY_NAM,0,100;DIST_CONG "DIST_CONG" true true false 2 Short 0 5,First,#,sgid_pcts_dissolved_spjoin6,DIST_CONG,-1,-1;DIST_STSEN "DIST_STSEN" true true false 2 Short 0 5,First,#,sgid_pcts_dissolved_spjoin6,DIST_STSEN,-1,-1;DIST_STASS "DIST_STASS" true true false 2 Short 0 5,First,#,sgid_pcts_dissolved_spjoin6,DIST_STASS,-1,-1;CITY_EST "CITY_EST" true true false 100 Text 0 0,First,#,sgid_pcts_dissolved_spjoin6,CITY_EST,0,100;DST_JRC "DST_JRC" true true false 10 Text 0 0,First,#,sgid_pcts_dissolved_spjoin6,DST_JRC,0,10;DST_USD "DST_USD" true true false 60 Text 0 0,First,#,sgid_schoolboarddist,DIST,0,60',"HAVE_THEIR_CENTER_IN", None, '')
 
 #: Joint the point data back to polygons (join sgid_pcts_dissolved.shp (polygon) to sgid_pcts_dissolved_spjoin7.shp (point)).
+print("join point data back to the polygon data.")
 sgid_pct_dissolved_joined = arcpy.JoinField_management(in_data="sgid_pcts_dissolved.shp", in_field="FID", join_table="sgid_pcts_dissolved_spjoin7.shp", join_field="TARGET_FID", fields="FID;Join_Count;TARGET_FID;PRECINCT;COUNTY_NUM;COUNTY_NAM;DIST_CONG;DIST_STSEN;DIST_STASS;CITY_EST;DST_JRC;DST_USD")
 
 #: APPEND THE JOINED DATA TO THE CIVIX SCHEMA (use the last spatially joined output file for the input)
-print("append the intermediate spatially joined data to the final output civix schema")
+print("append the intermediate spatially joined polygon data to the final output civix schema")
 arcpy.Append_management(sgid_pct_dissolved_joined, "utah_vp_civix.shp", "NO_TEST")
